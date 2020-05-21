1a3c020fe1f37ae7b08af6f6bda58d674316f9a8
songkangle <songkangle@trioly.com>
2020-05-21 10:13:35 +0800

    面试题解以及this指向问题

--- frontend/2020-05-15/03面试题图解.js
+++ frontend/2020-05-15/03面试题图解.js
@@ -59,3 +59,24 @@
     A(1);
     A(2);
 }
+
+
+// 第六道面试题
+{
+    var x = 3,
+    obj = {x:5};
+    obj.fn = (function(){
+        this.x *= ++x;
+        return function(y){
+            this.x *= ++x+y
+            console.log(x)
+        }
+    })()
+    var fn = obj.fn
+    obj.fn(6);
+    fn(4);
+    console.log(obj.x,x)
+}
+
+
+
